663713d65465292ec45de3ffd9c6e0f6e40a7ad1
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2025-03-07 09:40:17 +0800

    feat(@vben/web-antd): 能源管理前端开发

--- apps/web-energy-management/src/router/routes/external/energyConsumption.ts
+++ apps/web-energy-management/src/router/routes/external/energyConsumption.ts
@@ -29,6 +29,45 @@ const routes: RouteRecordRaw[] = [
     name: 'lossAnalysis',
     path: '/lossAnalysis',
   },
+  {
+    component: () =>
+      import(
+        '#/views/energyConsumptionStatistics/testAnalysis/productEnergyConsumption.vue'
+      ),
+    meta: {
+      icon: VBEN_LOGO_URL,
+      title: '产品用能',
+      ignoreAccess: true,
+    },
+    name: 'productEnergyConsumption',
+    path: '/productEnergyConsumption',
+  },
+  {
+    component: () =>
+      import(
+        '#/views/energyConsumptionStatistics/testAnalysis/systemEnergyConsumption.vue'
+      ),
+    meta: {
+      icon: VBEN_LOGO_URL,
+      title: '分项系统用能',
+      ignoreAccess: true,
+    },
+    name: 'systemEnergyConsumption',
+    path: '/systemEnergyConsumption',
+  },
+  {
+    component: () =>
+      import(
+        '#/views/energyConsumptionStatistics/testAnalysis/teamEnergyConsumption.vue'
+      ),
+    meta: {
+      icon: VBEN_LOGO_URL,
+      title: '班组用能',
+      ignoreAccess: true,
+    },
+    name: 'teamEnergyConsumption',
+    path: '/teamEnergyConsumption',
+  },
 ];
 
 export default routes;
