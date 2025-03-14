2b8135f9f403718fee05a369e63e2969c870a352
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2025-03-14 17:10:53 +0800

    fix(@vben/web-antd): 图标优化
    1. 修改bug, 图标在内网环境加载失败

--- apps/web-antd/src/main.ts
+++ apps/web-antd/src/main.ts
@@ -1,12 +1,30 @@
 import { initPreferences } from '@vben/preferences';
 import { unmountGlobalLoading } from '@vben/utils';
 
+// eslint-disable-next-line n/no-extraneous-import
+import { addCollection } from '@iconify/vue';
+
 import { overridesPreferences } from './preferences';
 
 /**
  * 应用初始化完成之后再进行页面加载渲染
  */
 async function initApplication() {
+  // region 加载图标
+
+  // 动态导入 @iconify/json/json 下的所有图标集
+  const iconCollections = import.meta.glob('@iconify/json/json/*.json', {
+    eager: true,
+  });
+
+  // 遍历所有图标集并注册
+  for (const path in iconCollections) {
+    const collection: any = iconCollections[path];
+    addCollection(collection.default);
+  }
+
+  // endregion
+
   // name用于指定项目唯一标识
   // 用于区分不同项目的偏好设置以及存储数据的key前缀以及其他一些需要隔离的数据
   const env = import.meta.env.PROD ? 'prod' : 'dev';
