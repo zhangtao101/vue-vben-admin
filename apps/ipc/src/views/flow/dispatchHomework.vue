19b8f99cd674d9e3a7c7aa4b035d88a9b65cd016
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2025-05-10 10:10:38 +0800

    docs(@vben/ipc): 注释添加

--- apps/ipc/src/views/flow/dispatchHomework.vue
+++ apps/ipc/src/views/flow/dispatchHomework.vue
@@ -20,13 +20,23 @@ const bottomNavigation = reactive([
 const checkedBottomNavigation = ref(bottomNavigation[0]);
 
 // endregion
+// 定义一个响应式变量，用于存储页面元素的宽度，默认值为 0
 const elementWidth = ref(0);
+
+// 在组件挂载完成后执行的操作
 onMounted(() => {
+  // 获取页面元素的宽度
+  // 使用 document.querySelector('#page') 获取页面元素，并通过 offsetWidth 获取其宽度
+  // 如果页面元素不存在，则返回 0
   elementWidth.value =
     (document.querySelector('#page') as any)?.offsetWidth || 0;
+
+  // 添加窗口大小调整事件监听器
+  // 当窗口大小发生变化时，触发 debounce 函数，延迟 500 毫秒后更新 elementWidth 的值
   window.addEventListener(
     'resize',
     debounce(() => {
+      // 更新页面元素的宽度
       elementWidth.value =
         (document.querySelector('#page') as any)?.offsetWidth || 0;
     }, 500),
