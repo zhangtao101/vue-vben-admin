ab832b3b6aa0f9d8ad48b1aef89e56f86f47a0b4
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2026-03-14 13:10:30 +0800

    feat(@vben/mes): 版本更新

--- apps/mes/src/store/auth.ts
+++ apps/mes/src/store/auth.ts
@@ -96,8 +96,7 @@ export const useAuthStore = defineStore('auth', () => {
   }
 
   async function fetchUserInfo() {
-    let userInfo: null | UserInfo = null;
-    userInfo = await getUserInfoApi();
+    const userInfo = await getUserInfoApi();
     userStore.setUserInfo(userInfo);
     return userInfo;
   }
