e27ee18fdd291c57a4f75bee7b6889980b8f8372
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2026-03-18 09:39:47 +0800

    feat(@vben/mes): 版本更新

--- apps/mes/src/api/productionReport/index.ts
+++ apps/mes/src/api/productionReport/index.ts
@@ -3,5 +3,6 @@ export * from './energyConsumptionCollectionDetails.service';
 export * from './productionFeedAudit.service';
 export * from './productionMonitoring';
 export * from './productRed.service';
+export * from './SMTPlantAdd.service';
 export * from './supplementaryFeedingOperation.service';
 export * from './workOrderParams.service';
