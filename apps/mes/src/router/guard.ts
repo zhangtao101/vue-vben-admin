4c26071e6a25e5ccb1d9734ffa7b9593e5dc2b09
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2026-09-23 15:02:01 +0800

    style(@vben/mes): 格式化路由相关代码
    - 调整 access.ts 与 guard.ts 代码空行

--- apps/mes/src/router/guard.ts
+++ apps/mes/src/router/guard.ts
@@ -72,7 +72,6 @@ async function generateDynamicRoutes(
   const userInfo = userStore.userInfo || (await authStore.fetchUserInfo());
   // 当前用户拥有的角色标识列表
   const userRoles = userInfo.roles ?? [];
-
   // 根据角色生成可访问的路由与菜单树
   const { accessibleMenus, accessibleRoutes } = await generateAccess({
     roles: userRoles,
